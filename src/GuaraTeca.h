e41c5dfeb1e5967d51431aa43e1333ca1d1ddbd4
JoaquimFlavio <joaquimflavio.quirino@gmail.com>
2017-10-19 20:45:23 -0300

    coreção dos cabeçalhos

---
+++ src/GuaraTeca.h
@@ -0,0 +1,34 @@
+//Copyright 2017 Joaquim Fl�vio Almeida Quirino Gomes, D�mis Carlos ----*
+//Fonseca Gomes, Marcos Dias da Concei��o e Diego de Castro Rodrigues---*
+//----------------------------------------------------------------------*
+//Este arquivo � parte da Biblioteca de Fun��es Guar�Teca---------------*
+//A Guar�Teca � um software livre; voc� pode redistribu�-lo e/ou--------*
+//modific�-lo sob os termos da Licen�a P�blica Geral GNU como publicada-*
+//pela Funda��o do Software Livre (FSF); na vers�o 3 da Licen�a,--------*
+//ou (a seu crit�rio) qualquer vers�o posterior.------------------------*
+//
+//Este programa � distribu�do na esperan�a de que possa ser �til,-------*
+//mas SEM NENHUMA GARANTIA; sem uma garantia impl�cita de ADEQUA��O-----*
+//a qualquer MERCADO ou APLICA��O EM PARTICULAR. Veja a-----------------*
+//Licen�a P�blica Geral GNU para mais detalhes.-------------------------*
+//
+//Voc� deve ter recebido uma c�pia da Licen�a P�blica Geral GNU junto---*
+//com este programa. Se n�o, veja <http://www.gnu.org/licenses/>--------*
+#ifndef GuaraTeca_H
+#define GuaraTeca_H
+
+//Tradutor para o usuario--------------------------
+#define Hardware 0
+#define OBR      1
+#define Aprender 2
+
+//Define functions biblios------------------------- 
+#if funcao == 0
+    #include <GuaraTeca_Hardware.h>
+#elif funcao == 1
+    #include <GuaraTeca_OBR.h>
+#elif funcao == 2
+    #include <GuaraTeca_Aprender.h>
+#endif
+
+#endif
